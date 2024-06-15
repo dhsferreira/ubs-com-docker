bb39384a20eefe40c7e4c33f1cb7b348c86f4654
dhsferreira <126525847+dhsferreira@users.noreply.github.com>
2024-06-14 21:21:36 -0300

    Redirecting news
    Redirecting news from the news screen to the sites they belong to

--- API UBS DEFINITIVO/my-app/Telas/Noticias/telaNoticias.tsx
+++ API UBS DEFINITIVO/my-app/Telas/Noticias/telaNoticias.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, Image, TouchableOpacity, ScrollView, Text } from 'react-native';
+import { View, Image, TouchableOpacity, ScrollView, Text, Linking } from 'react-native';
 import { useNavigation, DrawerActions } from '@react-navigation/native';
 import { styles } from './noticiasStyles';
 
 const images = [
-  { src: require('../assets/noticia1.png'), text: 'ARAUCÁRIA: UBS Industrial aumenta capacidade de atendimento' },
-  { src: require('../assets/noticia2.png'), text: 'Estudantes de Medicina realizam ações práticas na comunidade' },
-  { src: require('../assets/noticia3.png'), text: 'Jardim Antártica recebe nova UBS com capacidade para mais de 3,3 mil consultas médicas por mês' },
-  { src: require('../assets/noticia4.png'), text: 'Santarém 361 anos: UBS da Família é inaugurada no bairro Jutaí e vai beneficiar mais de 8 mil moradores' },
+  { src: require('../assets/noticia1.png'), text: 'Atende Já Dengue e Saúde Agora abrem quatro UBS neste sábado', link: 'https://guarulhosweb.com.br/atende-ja-dengue-e-saude-agora-abrem-quatro-ubs-neste-sabado/' },
+  { src: require('../assets/noticia2.png'), text: 'Paciente denuncia burocracia e demora no tratamento de um câncer na rede pública de saúde de SP', link: 'https://jovempan.com.br/programas/jornal-da-manha/paciente-denuncia-burocracia-e-demora-no-tratamento-de-um-cancer-na-rede-publica-de-saude-de-sp.html' },
+  { src: require('../assets/noticia3.png'), text: 'UBS Progresso realiza ação de prevenção de acidentes na infância em escola', link: 'https://www.pjf.mg.gov.br/noticias/view.php?modo=link2&idnoticia2=80932' },
+  { src: require('../assets/noticia4.png'), text: 'Com queda na procura, Sorocaba encerra atendimento exclusivo para dengue em UBSs Sentinelas', link: 'https://g1.globo.com/sp/sorocaba-jundiai/noticia/2024/06/13/com-queda-na-procura-sorocaba-encerra-atendimento-exclusivo-para-dengue-em-ubss-sentinelas.ghtml' },
 ];
 
 export default function HomeScreen() {
@@ -15,6 +15,10 @@ export default function HomeScreen() {
 
   const openDrawer = () => {
     navigation.dispatch(DrawerActions.openDrawer());
+  };
+
+  const handlePress = (link) => {
+    Linking.openURL(link);
   };
 
   return (
@@ -33,10 +37,14 @@ export default function HomeScreen() {
       </View>
       <ScrollView contentContainerStyle={styles.scrollContainer}>
         {images.map((item, index) => (
-          <View key={index} style={styles.imageContainer}>
+          <TouchableOpacity 
+            key={index} 
+            style={styles.imageContainer} 
+            onPress={() => item.link ? handlePress(item.link) : null}
+          >
             <Image source={item.src} style={styles.image} />
             <Text style={styles.imageText}>{item.text}</Text>
-          </View>
+          </TouchableOpacity>
         ))}
       </ScrollView>
     </View>
